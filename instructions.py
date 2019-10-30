8b30047ccd89d4f581ff25ad3eff0cb0892d8167
GMoves <thegmcode@gmail.com>
2019-10-30 13:30:28 -0400

    DocStrings

---
+++ instructions.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3 
+
+
+""" Teaching Ourselves to learn step by step instructions/ line by line ..."""
+
+# Define Some Tasks that involves "modern day challenges" with the endless amount of content floating around the world these days  being online with every procastination excuse in the world at your fingerprints 
